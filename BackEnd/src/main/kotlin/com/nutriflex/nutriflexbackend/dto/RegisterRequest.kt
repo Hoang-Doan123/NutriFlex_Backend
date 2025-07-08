0cab22515637b6a459bc57d7a7b5f795fe03964a
Hoang-Doan123 <hoangdd.ba12-078@st.usth.edu.vn>
2025-07-08 11:22:08 +0700

    Add goal val into personal_data collection

--- BackEnd/src/main/kotlin/com/nutriflex/nutriflexbackend/dto/RegisterRequest.kt
+++ BackEnd/src/main/kotlin/com/nutriflex/nutriflexbackend/dto/RegisterRequest.kt
@@ -12,5 +12,6 @@ data class RegisterRequest(
     val healthcareIssues: List<String>? = null,
     val injuries: List<String>? = null,
     val dietaryRestrictions: List<String>? = null,
-    val fitnessExperience: String? = null
+    val fitnessExperience: String? = null,
+    val goal: String? = null
 ) 
